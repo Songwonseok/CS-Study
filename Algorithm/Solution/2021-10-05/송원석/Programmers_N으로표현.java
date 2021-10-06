8f136cdaf9a66be65ba98cbdf4bdb4adc538c1b2
Songwonseok <dnjstjr0517@gmail.com>
2021-10-06 21:52:36 +0900

    Algo: 프로그래머스 연습문제 N으로 표현
    - DP

---
+++ Algorithm/Solution/2021-10-05/송원석/Programmers_N으로표현.java
@@ -0,0 +1,57 @@
+package Programmers.practice;
+
+import java.util.HashSet;
+
+public class Programmers_N으로표현 {
+	public int calculate(int num1, char symbol, int num2) {
+		switch(symbol) {
+			case '+' :
+				return num1 + num2;
+			case '-' :
+				return num1 - num2;
+			case '*' :
+				return num1 * num2;
+			default:
+				return num1 / num2;
+		}
+	}
+
+	public int solution(int N, int number) {
+		HashSet<Integer>[] dp = new HashSet[9];
+
+		for (int i = 1; i <= 8; i++) {
+			dp[i] = new HashSet<>();
+		}
+
+		for (int i = 1; i <= 8; i++) {
+			String repeatedN = Integer.toString(N).repeat(i);
+
+			dp[i].add(Integer.parseInt(repeatedN));
+
+			for (int j = 1; j < i; j++) {
+				HashSet<Integer> left = dp[j];
+				HashSet<Integer> right = dp[i - j];
+
+				for(int num1 : left) {
+					for(int num2 : right) {
+						dp[i].add(calculate(num1, '+', num2));
+						dp[i].add(calculate(num1, '-', num2));
+						dp[i].add(calculate(num1, '*', num2));
+						if(num2 != 0)
+							dp[i].add(calculate(num1, '/', num2));
+					}
+				}
+			}
+
+			if (dp[i].contains(number)) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
