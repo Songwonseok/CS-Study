b23f72bd79d0f93d208c2c27100bef739be82904
Songwonseok <ws.song@nts-corp.com>
2023-01-06 13:32:55 +0900

    Algo: LeetCode-1

--- Algorithm/Solution/2023/01/week1/SWS/0001-two-sum.java
+++ Algorithm/Solution/2023/01/week1/SWS/0001-two-sum.java
@@ -1,19 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap();        
+        
         int[] answer = new int[2];
-        HashMap<Integer, Integer> map = new HashMap();
         
         for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i])) {
-                answer[0] = map.get(nums[i]);
+            if(map.containsKey(target - nums[i])) {
+                answer[0] = map.get(target - nums[i]);
                 answer[1] = i;
                 break;
             }
             
-            map.put(target - nums[i], i);
+            map.put(nums[i], i); 
         }
         
-
         return answer;
+         
     }
 }
